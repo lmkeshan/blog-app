f0b0fe98c2e5319c4a7616395d0f4de078943e3f
lmkeshan <lmkeshan@gmail.com>
2026-07-25 11:28:36 +0530

    Verify database connection script using mysqli

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host     = "localhost";
+$username = "root";      
+$password = "";           
+$db_name  = "blog_app";
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
